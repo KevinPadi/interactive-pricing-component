b7070e7c6cf75664ebbf1299d69486b7a1efe8c3
Kevin <kevpadilla01@gmail.com>
2023-08-12 22:02:40 -0300

    fix: check icon

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Label ({ title }) {
   return (
     <div className='flex gap-3 items-center'>
-      <img src='./images/icon-check.svg' alt='check icon' />
+      <svg xmlns='http://www.w3.org/2000/svg' width='9' height='8'><path fill='none' stroke='#10D8C4' stroke-width='2' d='M1 4.134l1.907 1.908L7.949 1' /></svg>
       <p className='text-xs text-grayishBlue'>{title}</p>
     </div>
   )
